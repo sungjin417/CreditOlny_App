9cdec4d4830a24b5cf6a8974895ceb066c7931fd
Park Sung Jin <lbon321@naver.com>
2024-08-26 15:36:36 +0900

    creditonly_mobile

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import LocationProvider from "./LocationProvider";
+import WebViewComponent from "./WebViewComponent";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <LocationProvider>
+        <WebViewComponent />
+      </LocationProvider>
     </View>
   );
 }
@@ -13,8 +16,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
